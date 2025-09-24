2cfdc6024de8f0d8b45a6b061135d4478ecd5129
FernandoMoreti <fernandombolela@gmail.com>
2025-09-24 16:32:04 -0300

    feat(frontend) adicionando animação no img

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './App.css'
 import logo from "../public/logo.jpg"
+import { createScope, createDraggable, createSpring } from "animejs"
 
 function App() {
 
@@ -10,6 +11,25 @@ function App() {
   const [validar, setValidar] = useState(false)
   const [mostrar, setMostrar] = useState(false)
   const [mensagem, setMensagem] = useState(false)
+
+  const root = useRef(null);
+  const scope = useRef(null);
+
+  useEffect(() => {
+  
+    scope.current = createScope({ root }).add( self => {
+
+      createDraggable('.logo', {
+        container: [0, 0, 0, 0],
+        releaseEase: createSpring({ stiffness: 200 })
+      });
+
+    });
+    
+    // Properly cleanup all anime.js instances declared inside the scope
+    return () => scope.current.revert()
+    
+  }, []);
   
   async function handleSubmit(e) {
     e.preventDefault()
@@ -58,28 +78,29 @@ function App() {
 
   return (
     <>
-      <section className='flex flex-col h-screen bg-gradient-to-tr from-black to-purple-500'>
-        <p className={`absolute p-5 w-full transition-opacity duration-500 rounded-b-2xl ${mostrar ? ' opacity-100 ' : ' opacity-0 '}${validar ? 'bg-green-600' : 'bg-red-600'}`}>{validar ? "Editado com sucesso" : mensagem ? "Faltando credenciais" : "Não foi possivel editar"}</p>
-        <div className='flex-1 flex justify-center items-center bg-black/10'>
-          <div className='flex flex-col p-10 h-120 w-120 shadow-2xl shadow-gray-700 rounded-l-4xl'>
-            <h1 className='flex justify-center text-xl font-semibold'>Bem vindo ao Conversor WORKBANK</h1>
-            <form onSubmit={handleSubmit} className='flex flex-col gap-5' action="">
-              <p className='text-lg pt-5'>Importe o relatorio:</p>
-              <input className="border rounded-xl p-5 shadow-xl shadow-gray-700 cursor-pointer" onChange={(e) => setFile(e.target.files[0])} type="file" />
-              <p className='text-lg'>Escolha o banco:</p>
-              <select onChange={(e) => {setBanco(e.target.value)}} className='border rounded-xl p-5 shadow-xl shadow-gray-700 cursor-pointer' name="Banco" id="">
-                <option value="">Escolha um banco</option>
-                {bancos.map((banco) => (
-                  <option className='bg-black' value={banco}>{banco}</option>
-                ))}
-              </select>
-              <button className='border rounded-xl p-5 shadow-xl shadow-gray-700 transition-all durarion-400 cursor-pointer hover:bg-gray-300 hover:text-black' type='submit'>{loading ? "Carregando..." : "Criar arquivo"}</button>
-            </form>
+      <div ref={root}>
+        <section className='flex flex-col h-screen bg-gradient-to-tr from-black to-purple-500'>
+          <p className={`absolute p-5 w-full transition-opacity duration-500 rounded-b-2xl ${mostrar ? ' opacity-100 ' : ' opacity-0 '}${validar ? 'bg-green-600' : 'bg-red-600'}`}>{validar ? "Editado com sucesso" : mensagem ? "Faltando credenciais" : "Não foi possivel editar"}</p>
+          <div className='flex-1 flex justify-center items-center bg-black/10'>
+            <div className='flex flex-col p-10 h-120 w-120 shadow-2xl shadow-gray-700 rounded-l-4xl'>
+              <h1 className='flex justify-center text-xl font-semibold'>Bem vindo ao Conversor WORKBANK</h1>
+              <form onSubmit={handleSubmit} className='flex flex-col gap-5' action="">
+                <p className='text-lg pt-5'>Importe o relatorio:</p>
+                <input className="border rounded-xl p-5 shadow-xl shadow-gray-700 cursor-pointer" onChange={(e) => setFile(e.target.files[0])} type="file" />
+                <p className='text-lg'>Escolha o banco:</p>
+                <select onChange={(e) => {setBanco(e.target.value)}} className='border rounded-xl p-5 shadow-xl shadow-gray-700 cursor-pointer' name="Banco" id="">
+                  <option value="">Escolha um banco</option>
+                  {bancos.map((banco) => (
+                    <option className='bg-black' value={banco}>{banco}</option>
+                  ))}
+                </select>
+                <button className='border rounded-xl p-5 shadow-xl shadow-gray-700 transition-all durarion-400 cursor-pointer hover:bg-gray-300 hover:text-black' type='submit'>{loading ? "Carregando..." : "Criar arquivo"}</button>
+              </form>
+            </div>
+            <img className='logo w-120 rounded-r-4xl shadow-2xl shadow-gray-700' src={logo} alt="" />
           </div>
-          <img className='w-120 rounded-r-4xl shadow-2xl shadow-gray-700' src={logo} alt="" />
-        </div>
-      </section>
-
+        </section>
+      </div>
     </>
   )
 }
