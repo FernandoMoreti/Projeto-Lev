30673310ae96dac5e0c871fa1d98db3d5ecaf297
FernandoMoreti <fernandombolela@gmail.com>
2025-11-26 15:12:53 -0300

    feat(Frontend) Deixando menos feio o frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -106,22 +106,22 @@ function App() {
   return (
     <>
       <div ref={root}>
-        <section className='flex flex-col h-screen bg-gradient-to-tr from-black to-purple-500'>
+        <section className='flex flex-col h-screen'>
           <p className={`absolute p-5 w-full transition-opacity duration-500 rounded-b-2xl ${mostrar ? ' opacity-100 ' : ' opacity-0 '}${validar ? 'bg-green-600' : 'bg-red-600'}`}>{validar ? "Editado com sucesso" : mensagem ? "Faltando credenciais" : "Não foi possivel editar"}</p>
           <div className='flex-1 flex justify-center items-center bg-black/10'>
-            <div className='flex flex-col p-10 h-120 w-120 shadow-2xl shadow-gray-700 rounded-l-4xl'>
-              <h1 className='flex justify-center text-xl font-semibold'>Bem vindo ao Conversor WORKBANK</h1>
-              <form onSubmit={handleSubmit} className='flex flex-col gap-5' action="">
-                <p className='text-lg pt-5'>Importe o relatorio:</p>
-                <input className="border rounded-xl p-5 shadow-xl shadow-gray-700 cursor-pointer" onChange={(e) => setFile(e.target.files[0])} type="file" />
-                <p className='text-lg'>Escolha o banco:</p>
-                <select onChange={(e) => {setBanco(e.target.value)}} className='border rounded-xl p-5 shadow-xl shadow-gray-700 cursor-pointer' name="Banco" id="">
+            <div className='flex flex-col p-10 h-120 w-120 shadow-2xl bg-white shadow-gray-700 rounded-l-4xl'>
+              <h1 className='flex justify-center text-[22px] font-bold'>Bem vindo ao Conversor WORKBANK</h1>
+              <form onSubmit={handleSubmit} className='flex flex-col justify-center h-full' action="">
+                <p className='text-lg pt-5 font-semibold'>Importe o relatorio:</p>
+                <input className="border mt-2 rounded-xl p-5 shadow-md shadow-gray-700 cursor-pointer" onChange={(e) => setFile(e.target.files[0])} type="file" />
+                <p className='text-lg mt-5 font-semibold'>Escolha o banco:</p>
+                <select onChange={(e) => {setBanco(e.target.value)}} className='border mt-2 rounded-xl p-5 shadow-md shadow-gray-700 cursor-pointer' name="Banco" id="">
                   <option className='hidden'>Escolha um banco</option>
                   {bancos.map((banco) => (
-                    <option className='bg-black' value={banco}>{banco}</option>
+                    <option value={banco}>{banco}</option>
                   ))}
                 </select>
-                <button className='border rounded-xl p-5 shadow-xl shadow-gray-700 transition-all durarion-400 cursor-pointer hover:bg-gray-300 hover:text-black' type='submit'>{loading ? "Carregando..." : "Criar arquivo"}</button>
+                <button className='mt-5 bg-black text-white rounded-xl p-5 transition-all duration-300 cursor-pointer hover:bg-gray-600 hover:-translate-y-1' type='submit'>{loading ? "Carregando..." : "Criar arquivo"}</button>
               </form>
             </div>
             <img className='logo w-120 rounded-r-4xl shadow-2xl shadow-gray-700' src={logo} alt="" />
