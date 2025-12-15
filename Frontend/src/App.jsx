af1ea12eb82c5014909af4b2892e69802921893d
FernandoMoreti <fernandombolela@gmail.com>
2025-12-15 16:03:49 -0300

    feat(PHTECH) Adicionando o banco PHTECH no site

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.https://flask-backend-ipg8.onrender.com:5000/executar", {
+      const response = await fetch("https://flask-backend-ipg8.onrender.com/executar", {
         method: "POST",
         body: formData,
       })
