b9ed9087b3a68f1856fb61c520aeed080d40a618
FernandoMoreti <fernandombolela@gmail.com>
2026-01-14 08:25:53 -0300

    feat(utils) Testando utilss no site: v1

---
+++ Backend/app/utils.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+col_opcoes = [
+   "NUM_BANCO",
+   "NOM_BANCO",
+   "NUM_PROPOSTA",
+   "NUM_CONTRATO",
+   "NOM_CLIENTE",
+   "COD_CPF_CLIENTE",
+   "DSC_PRODUTO",
+   "DSC_SITUACAO_BANCO",
+   "DSC_OBSERVACAO",
+   "DAT_CREDITO",
+   "VAL_BRUTO",
+   "VAL_LIQUIDO",
+   "VAL_SALDO_REFINANCIAMENTO",
+   "VAL_BASE_COMISSAO",
+   "VAL_COMISSAO",
+   "PCL_COMISSAO",
+   "DSC_TIPO_COMISSAO",
+   "COD_LOJA",
+   "COD_UNIDADE_EMPRESA",
+   "COD_BANCO",
+   "COD_TIPO_PROPOSTA_EMPRESTIMO",
+   "DSC_TIPO_PROPOSTA_EMPRESTIMO",
+   "NIC_CTR_USUARIO",
+   "COD_PRODUTO",
+   "COD_PRODUTOR_VENDA",
+   "COD_PRODUTOR_VENDA_BANCO",
+   "COD_TIPO_COMISSAO",
+   "COD_SITUACAO_EMPRESTIMO",
+   "QTD_PARCELA",
+   "NUM_PARCELA_DIFERIDA_EMPRESA",
+   "DAT_EMPRESTIMO",
+   "DAT_CONFIRMACAO",
+   "DAT_ESTORNO",
+   "DAT_CTR_INCLUSAO",
+   "TIPO_COMISSAO_BANCO",
+   "PCL_TAXA_EMPRESTIMO"
+]
+
+def createDataframe():
+    newDataFrame = pd.DataFrame(columns=col_opcoes)
+    return newDataFrame
+
+def validDf(df, infos):
+
+    if not isinstance(df, pd.DataFrame):
+        return"Erro: A entrada não é um DataFrame válido."
+
+    colunas_origem_presentes = all(col_origem in df.columns for col_origem in infos.keys())
+    
+    if not colunas_origem_presentes:
+        return"ErroColunas"
+    
+def inputValueColumns(df, df_novo, infos):
+    for col_origem, col_destino in infos.items():
+        if col_origem in df.columns:
+            df_novo[col_destino] = df[col_origem]
+
+    return df_novo
