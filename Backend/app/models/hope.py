2d7b23facd7af05005dbd3d5e92f2daad88c2e21
FernandoMoreti <fernandombolela@gmail.com>
2026-08-13 12:04:46 -0300

    UPDATE columns Hope

--- Backend/app/models/hope.py
+++ Backend/app/models/hope.py
@@ -12,7 +12,7 @@ class Hope(Bank):
     def readArchive(self, df):
         try:
             df = pd.read_excel(df, header=0)
-            df = df[pd.notna(df["Valor Base do Contrato"])]
+            df = df[pd.notna(df["VALOR_BASE_CALCULO"])]
             df = df.iloc[:-1]
             return df
         except Exception:
@@ -29,11 +29,10 @@ class Hope(Bank):
             df = self.readArchive(df)
 
             infos ={
-                "Número Ade":"NUM_PROPOSTA",
-                "Data do Fechamento":"DAT_CREDITO",
-                "Valor Bruto":"VAL_BASE_COMISSAO",
-                "% Comissão": "PCL_COMISSAO",
-                "Valor":"VAL_COMISSAO",
+                "NUMERO_PROPOSTA":"NUM_PROPOSTA",
+                "DATA_PGTO_CLIENTE":"DAT_CREDITO",
+                "VALOR_BASE_CALCULO":"VAL_BASE_COMISSAO",
+                "VALOR_PAGO":"VAL_COMISSAO",
             }
 
             logger.info("Validando DataFrame")
